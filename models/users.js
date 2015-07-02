904703283a5cea44ab1ff26ab8a6376375c9e657
Maxime Heckel <heckelmaxime@gmail.com>
2015-07-02 19:10:13 +0200

    add docker-compose.test.yml with basic test for tutum builder

--- models/users.js
+++ models/users.js
@@ -1,13 +1,11 @@
 var mongoose = require('mongoose');
 var File = require('./composeFiles.js');
-var findOneOrCreate = require('mongoose-find-one-or-create');
 
 var schema = new mongoose.Schema({
     userId: {type: String},
     username: {type: String, index: {unique: true, dropDups: true}},
     profileUrl: {type: String},
     files: [File]
-})
+});
 
-schema.plugin(findOneOrCreate);
 module.exports = mongoose.model('User', schema);
