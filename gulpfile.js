2e931446b516f856fb3667256fd4694133d6c3e3
HECKEL Maxime <heckelmaxime@gmail.com>
2015-08-20 12:05:25 +0200

    clean dist folder for gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,23 @@ var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
 var nodemon = require('gulp-nodemon');
+var plumber = require('gulp-plumber');
+var shell = require('gulp-shell');
 
 // tasks
-gulp.task('lint', function() {
+gulp.task('lint', ['clean'], function() {
   gulp.src(['./src/**/*.js', '!./src/lib/**'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'));
 });
-gulp.task('clean', function() {
-    gulp.src('./dist/*')
-      .pipe(clean({force: true}));
-});
+/*gulp.task('clean', function() {
+    gulp.src('./dist/**')
+      .pipe(clean());
+});*/
+gulp.task('clean', shell.task([
+  'rm -r ./dist/**'
+]));
 gulp.task('minify-css', function() {
   var opts = {comments:true,spare:true};
   gulp.src(['./src/**/*.css', '!./src/lib/**'])
@@ -46,12 +51,7 @@ gulp.task('copy-img-files', function () {
   gulp.src('./src/img/*')
     .pipe(gulp.dest('dist/img'));
 });
-gulp.task('connect', function () {
-  connect.server({
-    root: 'src/',
-    port: 8888
-  });
-});
+
 gulp.task('connectDist', function () {
   nodemon({
     script: 'server.js',
@@ -60,19 +60,11 @@ gulp.task('connectDist', function () {
   });
 });
 
-/*gulp.task('connectDist', function () {
-  connect.server({
-    root: 'dist/',
-    port: 9999
-  });
-});*/
-
-
 // default task
 gulp.task('default',
-  ['lint', 'connect']
+  ['lint', 'minify-css', 'minify-js', 'copy-img-files', 'copy-html-files', 'copy-bower-components', 'connectDist']
 );
 // build task
 gulp.task('build',
-  ['lint', 'minify-css', 'minify-js', 'copy-html-files','copy-img-files', 'copy-bower-components', 'connectDist']
+  ['lint', 'minify-css', 'minify-js', 'copy-img-files', 'copy-html-files', 'copy-bower-components']
 );
