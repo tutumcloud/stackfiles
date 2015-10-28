7d176bb06bc1fbc6e5ff1a0bb9ad42b6074461ef
HECKEL Maxime <heckelmaxime@gmail.com>
2015-10-28 16:33:42 +0100

    add download button to detail view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -245,6 +245,7 @@ angular.module('registry.controllers', [])
         API.getYAMLFile(data._id, data.projectName, data.path).success(function(yamlData, status, headers, config){
             $scope.composeFile = yamlData;
             $scope.images = processImages(data.images);
+            $scope.downloadLink = "https://github.com/" + data.user + "/" + data.projectName + "/raw/" + data.branch + "/" + data.path + "/docker-compose.yml";
             $scope.loaded = true;
         }).error(function(data, status, headers, config){
             $scope.composeFile = "Unable to fetch tutum.yml from Github repository. Please select a repository that contains a tutum.yml or a docker-compose.yml file";
